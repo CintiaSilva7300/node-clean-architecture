99900b6a780dc7116791f29eb7a4f41e1bc444ae
CINTIA SILVA <cintiasilva7300@gmail.com>
2023-09-22 16:16:49 -0300

    refactor: make SignUpController conforms to a protocol

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -35,12 +35,26 @@ describe('SignUp Controller', () => {
         const httpRequest = {
             body: {
                 name:"any_name",
-                email:"any_email",
+                email:"any_email@mail.com",
                 passwordConfirmation: "any_password",
             }
         }
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    })
+
+    test('Shoult return 400 if no passwordConfirmation is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name:"any_name",
+                email:"any_email@mail.com",
+                password: "any_password",
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
     })
 })
